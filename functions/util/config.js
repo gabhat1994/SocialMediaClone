58275758b10b5138998190f14b2ca662af0fb9db
stamic1 <bhatgaurav96@gmail.com>
2020-10-05 12:49:28 +0530

    code refactoringand organising

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports  = {
+    apiKey: "AIzaSyC_PdR373HbKIhXWMtEGUT47UCqBFZjCBY",
+    authDomain: "socialape-66286.firebaseapp.com",
+    databaseURL: "https://socialape-66286.firebaseio.com",
+    projectId: "socialape-66286",
+    storageBucket: "socialape-66286.appspot.com",
+    messagingSenderId: "903626918407",
+    appId: "1:903626918407:web:be2f5b3408832ebcf478a2",
+    measurementId: "G-9JQ88QQZK3",
+  };
---
+++ functions/util/validators.js
@@ -0,0 +1,28 @@
+const isEmpt = (string) => {
+  if (string.trim() === "") return true;
+  else return false;
+};
+
+exports.validateSignUpData = (data) => {
+  let errors = {};
+
+  if (isEmpt(data.email)) {
+    errors.email = "Must not be empty";
+  }
+
+  if (isEmpt(data.password)) {
+    errors.password = "Must not be empty";
+  }
+  if (data.password !== data.confirmPassword) {
+    errors.confirmPassword = "Passwords must match";
+  }
+
+  if (isEmpt(data.handle)) {
+    errors.handle = "Must not be empty";
+  }
+
+  return {
+    errors,
+    valid: Object.keys(errors).length === 0 ? true : false,
+  };
+};
